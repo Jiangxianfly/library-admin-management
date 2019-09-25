b2e879b2abd32637c4ea6a8cafd1a743b6406a51
Jiangxf <13295690718@163.com>
2019-09-25 17:14:20 +0800

    图书管理系统代码上传Git版本控制

---
+++ src/test/java/insert/BookClassServiceTest.java
@@ -0,0 +1,29 @@
+package insert;
+
+import com.chen.PLoveLibrary.domain.BookClass;
+import com.chen.PLoveLibrary.mybatis.BookClassMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath:config/mybatisConfig/spring-mybatis.xml","classpath:config/springConfig/applicationContext.xml"})
+public class BookClassServiceTest {
+
+    @Resource
+    BookClassMapper bookClassMapper;
+
+    @Test
+    public void insertBookClass(){
+        BookClass bookClass=new BookClass();
+        for (int i = 5; i < 20; i++) {
+            bookClass.setBkCatalog("TP"+i);
+            bookClass.setBkClassName("自动化");
+            bookClassMapper.insertBookClass(bookClass);
+        }
+    }
+}
